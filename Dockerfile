2220fa185e7fff6467154ecafd1d47ffc0140558
Jorge Sanchez <sandix390@gmail.com>
2021-07-06 13:39:37 -0500

    add sql alchmey support

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,8 @@ RUN pip3 install -r requirements.txt
 
 COPY . /myportfolio
 
+RUN chmod u+x ./entrypoint.sh
+
+ENTRYPOINT ["./entrypoint.sh"]
+
 CMD ["gunicorn", "wsgi:app", "-w 4", "-b 0.0.0.0:80"]
--- app/__init__.py
+++ app/__init__.py
@@ -1,91 +1,85 @@
 from flask import Flask, render_template, url_for, json, Response, request
 import os
-from . import db
+#from . import db
 
 #Login and register
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.db import get_db
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
+    user=os.getenv('POSTGRES_USER'),
+    passwd=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST'),
+    port=5432,
+    table=os.getenv('POSTGRES_DB'))
 
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-db.init_app(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-# code by kOssi (https://stackoverflow.com/questions/21133976/flask-load-local-json)
-SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-SITE_FOLDER = "static/data"
-
-@app.route('/')
-def index():
-    return render_template('landing_page.html')
-
-@app.route('/profile/<name>')
-def profile(name):
-    json_url = os.path.join(SITE_ROOT, SITE_FOLDER, f"{name}.json")
-    data = json.load(open(json_url))
-    return render_template('profile.html', data=data)
-
-# AWS Health endpoint
-@app.route('/health', methods=['GET'])
-def health():
-    return Response(status=200)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-#Register endpoint
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return f"User {username} created successfully"
         else:
             return error, 418
 
-    ## TODO: Return a restister page
+    # TODO: Return a restister page
     return "Register Page not yet implemented", 501
 
 
-#Login endpoint
 @app.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
             error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user.password, password):
             error = 'Incorrect password.'
 
         if error is None:
-            return "Login Successful", 200 
+            return "Login Successful", 200
         else:
             return error, 418
-    
-    ## TODO: Return a login page
+
+    # TODO: Return a login page
     return "Login Page not yet implemented", 501
